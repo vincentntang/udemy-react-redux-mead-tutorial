d32be5bfbeabf24c725a530f88b70c2e5fa23b30
Vincent <10290348+Kagerjay@users.noreply.github.com>
2019-01-06 00:28:16 -0500

    92- reducers

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -19,8 +19,13 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
 };
 
 // Filters Reducer
-// text => 'text, sortBy, startDate, endDate
-const filtersReducerDefaultState = [];
+// text => 'text, sortBy=> 'date', startDate=> undefined, endDate => undefined
+const filtersReducerDefaultState = {
+  text: "",
+  sortBy: "date",
+  startDate: undefined,
+  endDate: undefined
+};
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
     default:
