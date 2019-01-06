ed08015ff4f49816b9103d6ebe4d167d4290dbf9
Vincent <10290348+Kagerjay@users.noreply.github.com>
2019-01-05 23:18:30 -0500

    86_beforreLesson

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,15 +1,31 @@
 import { createStore } from "redux";
 
-const store = createStore((state = { count: 0 }) => {
-  return state;
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1
+      };
+    case "DECREMENT":
+      return {
+        count: state.count - 1
+      };
+    default:
+      return state;
+  }
 });
 
-// this.setState((state = { count: 10 }) => {
-//   return prevState;
-// });
-
+// Actions - than an object that gets sent to the store
 console.log(store.getState());
 
-// Actions
+// I'd like to increment the count
+store.dispatch({
+  type: "INCREMENT"
+});
 
-// console.log("101");
+store.dispatch({
+  type: "INCREMENT"
+});
+
+// I'd like to reset the count to zero
+console.log(store.getState());
