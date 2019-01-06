6d4ec3da75995fe44066394d1a3e60f4c6c9a837
Vincent <10290348+Kagerjay@users.noreply.github.com>
2019-01-05 23:41:54 -0500

    Array Destructuring - 89

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -17,14 +17,28 @@
 //   console.log(`its a ${temperature} in ${city}`);
 // }
 
-const book = {
-  title: "Ego is the Enemy",
-  author: "Ryan Holiday",
-  publisher: {
-    name: "Penguin"
-  }
-};
+// const book = {
+//   title: "Ego is the Enemy",
+//   author: "Ryan Holiday",
+//   publisher: {
+//     name: "Penguin"
+//   }
+// };
 
-const { name: publisherName } = book.publisher;
+// const { name: publisherName } = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
+
+// Array destructuring
+
+const address = ["1299 S Junpier Street", "Philly", "Penn", "19147"];
+
+const [, city, state = "New York"] = address;
+
+console.log(`You are in ${state} in ${city}`);
+
+const item = ["Coffee(hot)", "$2", "$2.5", "$2.75"];
+
+const [itemName, , mediumPrice] = item;
+
+console.log(`A medium ${itemName} cost ${mediumPrice}`);
