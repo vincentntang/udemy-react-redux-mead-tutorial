42361d1ac4d1b8373acb5329533d005165d2f4c9
Vincent <10290348+Kagerjay@users.noreply.github.com>
2019-01-06 11:34:01 -0500

    initialization of section 96

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 console.log(__dirname, "public");
 
 module.exports = {
-  entry: "./src/playground/redux-expensify.js",
+  entry: "./src/playground/redux-expensify2-reduce.js",
   output: {
     path: path.join(__dirname, "public"),
     filename: "bundle.js"
