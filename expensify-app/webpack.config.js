e357eb526979b2aff02621fb0d0636647de1bf45
Vincent <10290348+Kagerjay@users.noreply.github.com>
2019-01-06 20:28:59 -0500

    initialize 100

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 console.log(__dirname, "public");
 
 module.exports = {
-  entry: "./src/app.js",
+  entry: "./src/playground/hoc.js",
   output: {
     path: path.join(__dirname, "public"),
     filename: "bundle.js"
