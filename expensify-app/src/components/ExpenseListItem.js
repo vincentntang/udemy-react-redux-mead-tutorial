2a399263006cb2fb9f832fd54db76931759a186d
Vincent <10290348+Kagerjay@users.noreply.github.com>
2019-01-07 23:05:26 -0500

    103- No idea why redux works this way

--- expensify-app/src/components/ExpenseListItem.js
+++ expensify-app/src/components/ExpenseListItem.js
@@ -2,14 +2,31 @@
 // description, amount, createdAt
 
 import React from "react";
+import { connect } from "react-redux";
+import { removeExpense } from "../actions/expenses";
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
-  <div>
-    <h3>{description}</h3>
-    <p>
-      {amount} - {createdAt}
-    </p>
-  </div>
-);
+// connect function intercepts the props coming from {...expense} and passes those along with dispatch
+// into component it wraps
 
-export default ExpenseListItem;
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => {
+  console.log(id);
+  return (
+    <div>
+      <h3>{description}</h3>
+      <p>
+        {amount} - {createdAt}
+      </p>
+      <button
+        onClick={() => {
+          dispatch(removeExpense({ id }));
+        }}
+      >
+        Remove
+      </button>
+    </div>
+  );
+};
+
+export default connect()(ExpenseListItem);
+// you need connect to get the dispatch call, try changing it to "ExpenseListItem" instead of
+// connect()(ExpenseListItem) to see difference
