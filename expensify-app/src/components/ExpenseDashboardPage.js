2b96e09a49b0c5fb3b7fe597371f22f0a39d8f7d
Vincent <10290348+Kagerjay@users.noreply.github.com>
2019-01-07 22:47:00 -0500

    103 - start

--- expensify-app/src/components/ExpenseDashboardPage.js
+++ expensify-app/src/components/ExpenseDashboardPage.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ExpenseList from "./ExpenseList";
+import ExpenseListFilters from "./ExpenseListFilters";
 
 const ExpenseDashboardPage = () => (
   <div>
+    <ExpenseListFilters />
     <ExpenseList />
-    This is from the dashboard component
   </div>
 );
 
