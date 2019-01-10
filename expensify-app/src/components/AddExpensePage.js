0cfd2fc3f98e3fb9af0a0caba52316525381488a
Vincent <10290348+Kagerjay@users.noreply.github.com>
2019-01-09 22:51:09 -0500

    107-finished

--- expensify-app/src/components/AddExpensePage.js
+++ expensify-app/src/components/AddExpensePage.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
 import ExpenseForm from "./ExpenseForm";
+import { addExpense } from "../actions/expenses";
 
-const AddExpensePage = () => (
+// expenseForm onsubmit={expense}......
+// explaination from video 107 udemy
+//  All we're doing is passing a function from AddExpensePage into ExpenseForm and then calling it in ExpenseForm with the expense as an argument.
+const AddExpensePage = props => (
   <div>
     <h1>Add Expense</h1>
-    <ExpenseForm />
+    <ExpenseForm
+      onSubmit={expense => {
+        props.dispatch(addExpense(expense));
+        props.history.push("/"); // hijacks back button
+      }}
+    />
   </div>
 );
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);
