2b858d7e5fb03c0ef63ed52a1c412025f148f402
Vincent <10290348+Kagerjay@users.noreply.github.com>
2019-02-03 23:33:58 -0500

    122- user interaction

--- expensify-app/src/tests/components/ExpenseForm.test.js
+++ expensify-app/src/tests/components/ExpenseForm.test.js
@@ -13,3 +13,54 @@ test('should render ExpenseForm correctly with expense data', () => {
   const wrapper = shallow(<ExpenseForm expense={expenses[1]}/>);
   expect(wrapper).toMatchSnapshot();
 })
+
+test('shoulder render error for invalid form submissions', () => {
+  const wrapper = shallow(<ExpenseForm/>);
+  expect(wrapper).toMatchSnapshot();
+  wrapper.find('form').simulate('submit', {
+    preventDefault:() => {}
+  });
+  expect(wrapper.state('error').length).toBeGreaterThan(0);
+  expect(wrapper).toMatchSnapshot();
+})
+
+test('should set description on input change', ()=> {
+  const value = 'New description';
+  const wrapper = shallow(<ExpenseForm/>);
+  wrapper.find('input').at(0).simulate('change', { // (first input)
+    target: {value}
+  });
+  expect(wrapper.state('description')).toBe(value);
+})
+
+// should set note on textarea change
+test('should set note on textarea change', () => {
+  const value = 'New note value';
+  const wrapper = shallow(<ExpenseForm/>);
+  wrapper.find('textarea').simulate('change', {
+    target: { value}
+  });
+  expect(wrapper.state('note')).toBe(value);
+})
+
+// should set amount if valid input
+// 23.50
+test('should set amount if valid input', () => {
+  const value = '23.50';
+  const wrapper = shallow(<ExpenseForm/>);
+  wrapper.find('input').at(1).simulate('change', {
+    target: {value}
+  })
+  expect(wrapper.state('amount')).toBe(value);
+})
+
+// should not set amount if invalid input
+// 12.122
+test('should not set amount if invalid input', () => {
+  const value = '12.122';
+  const wrapper = shallow(<ExpenseForm/>);
+  wrapper.find('input').at(1).simulate('change', {
+    target: {value}
+  })
+  expect(wrapper.state('amount')).toBe(''); // set no value if invalid
+})
